7d467e2c70c32d1230b66c704ef4ee9533481592
TPI Marienburg <david.katz@tprojects.org>
2018-11-13 13:04:13 -0800

    Added GUI
    Added a basic GUI using tkinter

---
+++ GUI.py
@@ -0,0 +1,41 @@
+__author__ = "dkatz"
+__version__ = "rc1"
+
+from tkinter import ttk
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from AnnualReviewsReportRunnerv2 import FindReviewDates
+
+def process():
+    a = FindReviewDates(month.get(), year.get(), path.get())
+    a.processing()
+
+def open():
+    files = askopenfilename()
+    path.set(files)
+
+root = Tk()
+root.title("Annual Reviews Report Runner")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+path = StringVar()
+month = StringVar()
+year = StringVar()
+
+ttk.Label(mainframe, text="Path:").grid(column=1, row=1,sticky=W)
+ttk.Entry(mainframe, textvariable=path, width=100).grid(column=1, row=2, columnspan=4, sticky=W)
+ttk.Label(mainframe, text="Enter the month and year the report was run, not the month they are to be sent out.").grid(
+    column=1, row=3, columnspan=4)
+ttk.Label(mainframe, text="Month(M):",).grid(column=1, row=4, sticky=E)
+ttk.Entry(mainframe, textvariable=month).grid(column=2, row=4, sticky=W)
+ttk.Label(mainframe, text="Year(YYYY):").grid(column=3, row=4, sticky=E)
+ttk.Entry(mainframe, textvariable=year).grid(column=4, row=4, sticky=W)
+
+ttk.Button(mainframe, text="Process", command=process).grid(column=3, row=5, sticky=E)
+ttk.Button(mainframe, text="Open", command=open).grid(column=4, row=5, sticky=E)
+
+root.mainloop()
--- annualreviews.py
+++ annualreviews.py
@@ -1,145 +1,180 @@
 __author__ = 'David Katz'
-__version__ = 'rc1'
+__version__ = 'rc2'
 
 '''Currently this program throws and error when it encounters months Nov or Dec (as input by the user)  This will need
-to be fixed to ensure that this program can be used without modifications in the future'''
+to be fixed to ensure that this program can be used without modifications in the future
 
-from xlrd import open_workbook, xldate_as_tuple
-from datetime import date, datetime, time
-from xlutils.copy import copy
-from xlwt import Workbook, easyxf
+Additionally this program needs some serious cleaning up for PEP-8 compliance as well as elimination of deeply nested
+conditionals and use of global variables.  This is ugly code that just happens to work but will be hard to maintain
+going forward.'''
+
 import re
 
-rb = open_workbook(path)
-sheet = rb.sheet_by_index(0)
-wb = copy(rb)
-editsheet = wb.add_sheet('Processed')
-m = int(raw_input('Month(m):'))
-y = int(raw_input('Year(yyyy):'))
-month = 0
+from tkinter.filedialog import asksaveasfilename
+from xlrd import open_workbook
+from xlrd import xldate_as_tuple
+from xlutils.copy import copy
+from xlwt import easyxf
+
 wmonth = []
-year = 0
+month = 0
 wyear = []
+year = 0
 
+def processing(path, m, y):
+    file_path = str(path)
+    rb = open_workbook(file_path)
+    sheet = rb.sheet_by_index(0)
+    wb = copy(rb)
+    editsheet = wb.add_sheet('Processed')
+    month_entry = int(m)
+    year_entry = int(y)
 
-'''the workingMonth function advances the month by two respecting the 12 month year'''
-def workingMonth():
-    global wmonth
-    global month
-    if m == 11:
-        month = 1
-        wmonth.append(1)
-    elif m == 12:
-        month = 2
-        wmonth.append(2)
-    else:
-        c = m + 2
-        month = c
-        wmonth.append(c)
-
-'''the workingYear function advances they wyear variable by 1 in the event that the wmonth is Dec. or Nov.'''
-def workingYear():
-    global year
-    if (month == 11) or (month == 12):
-        year = y + 1
-    else:
-        year = y
-
-'''dateCheck will reduce a date tuple to only the month data then look at a given date and return 'true' if the date's
-month is equal to the working month'''
-def dateCheck(d):
-    global wmonth
-    dlist = list(d)
-    dlist.pop(0)
-    dlist.pop(3)
-    dlist.pop(2)
-    dlist.pop(2)
-    dlist.pop(1)
-    #print dlist
-    #print wmonth
-    if dlist == wmonth:
-        return True
-    else:
-        return False
-
-def yearCheck(y):
-    dlist = list(y)
-    dlist.pop(1)
-    dlist.pop(1)
-    dlist.pop(1)
-    dlist.pop(1)
-    dlist.pop(1)
-    if dlist == wyear:
-        return True
-    else:
-        return False
-
-'''dateConvert and yearConvery change excel date codes into date tuples so that python can read/process them'''
-def dateConvert(r,c):
-    #print xldate_as_tuple(sheet.cell(r,c).value, rb.datemode)
-    date_value = xldate_as_tuple(sheet.cell(r,c).value, rb.datemode)
-    return dateCheck(date_value)
-
-def yearConvert(r,c):
-    date_value = xldate_as_tuple(sheet.cell(r,c).value, rb.datemode)
-    return yearCheck(date_value)
-
-'''writeSheet writes data to the "processed" sheet'''
-def writeSheet():
-    row = 3
-    header = 'Required Annual Review Assessments- by %d / %d' % (month, year)
-    editsheet.write(0,0, header, easyxf('font: height 400;'))
-    for c in range(0,sheet.ncols):
-        if c == 7:
-            editsheet.write(2, 0,sheet.cell_value(rowx = 2, colx=c))
+    def working_month():
+        """
+        the workingMonth function advances the month by two respecting the 12 month year
+        :return:
+        """
+        global wmonth
+        global month
+        if month_entry == 11:
+            month = 1
+            wmonth.append(1)
+        elif month_entry == 12:
+            month = 2
+            wmonth.append(2)
         else:
-            editsheet.write(2, c+1, sheet.cell_value(rowx = 2, colx=c))
-    for r in range (3,sheet.nrows):
-        if dateConvert(r,3) == True:
-            print "POW!!!"
-            col = 0
-            for c in range(0,sheet.ncols):
-                #print r
-                #print c
-                #print sheet.cell_value(rowx = r, colx = c)
-                if (c == 4) and (sheet.cell_value(rowx = r, colx = 4) != ''):
-                    if (yearConvert(r,4) == True) and (dateCheck(dateConvert(r,4)) == True):
-                        #print "ping!"
-                        editsheet.write(row,c+1,sheet.cell_value(rowx = r, colx = c),easyxf('pattern: pattern solid_fill,fore_colour yellow;' ))
-                        col += 1
-                    else:
-                        editsheet.write(row,c+1,sheet.cell_value(rowx = r, colx = c))
-                elif (c == 5):
-                    if (yearConvert(r,5) == False) and (dateConvert(r,5) == True):
-                        #print "pong!"
-                        editsheet.write(row, c+1, sheet.cell_value(rowx = r, colx = c),easyxf('pattern: pattern solid_fill, fore_colour yellow;'))
-                        col += 1
-                    else:
-                        editsheet.write(row,c+1,sheet.cell_value(rowx = r, colx = c))
-                elif (c == 7):
-                    try:
-                        g = re.compile(ur'([A-Z][A-Z])')
-                        p = re.search(g,sheet.cell_value(rowx = r, colx = c))
-                        print p.group()
-                    except Exception:
-                        editsheet.write(row,0,sheet.cell_value(rowx = r, colx = c))
-                    except AttributeError:
-                        editsheet.write(row,0,sheet.cell_value(rowx = r, colx = c))
-                    else:
-                        g = re.compile(ur'([A-Z][A-Z])')
-                        p = re.search(g,sheet.cell_value(rowx = r, colx = c))
-                        print r
-                        print c
-                        editsheet.write(row,0,p.group())
-                else:
-                    print "BONK!"
-                    editsheet.write(row,c+1,sheet.cell_value(rowx = r, colx = c))
-                    col += 1
-            row += 1
-        else:
-            print "OOOF!"
+            c = month_entry + 2
+            month = c
+            wmonth.append(c)
 
-workingMonth()
-workingYear()
-writeSheet()
-wb.save('Required Annual Reviews Assessments (Processed).xls')
+    def working_year():
+        """
+        the workingYear function advances they wyear variable by 1 in the event that the wmonth is Dec. or Nov.
+        :return:
+        """
+        global year
+        if (month == 11) or (month == 12):
+            year = year_entry + 1
+        else:
+            year = year_entry
+
+    def datecheck(d):
+        """
+        dateCheck will reduce a date tuple to only the month data then look at a given date and return 'true' if the
+        date's month is equal to the working month
+        :param d:
+        :return:
+        """
+        global wmonth
+        dlist = list(d)
+        dlist.pop(0)
+        dlist.pop(3)
+        dlist.pop(2)
+        dlist.pop(2)
+        dlist.pop(1)
+        print(dlist)
+        print(wmonth)
+        if dlist == wmonth:
+            return True
+        else:
+            return False
+
+    def yearcheck(y):
+        dlist = list(y)
+        dlist.pop(1)
+        dlist.pop(1)
+        dlist.pop(1)
+        dlist.pop(1)
+        dlist.pop(1)
+        if dlist == wyear:
+            return True
+        else:
+            return False
+
+    def dateconvert(r, c):
+        """
+        dateConvert and yearConvert change excel date codes into date tuples so that python can read/process them
+        :param r:
+        :param c:
+        :return:
+        """
+        print(xldate_as_tuple(sheet.cell(r, c).value, rb.datemode))
+        date_value = xldate_as_tuple(sheet.cell(r, c).value, rb.datemode)
+        return datecheck(date_value)
+
+    def yearconvert(r, c):
+        print("row: {}; coll: {}".format(r, c))
+        date_value = xldate_as_tuple(sheet.cell(r, c).value, rb.datemode)
+        return yearcheck(date_value)
+
+    def writesheet(sheet, editsheet):
+        """
+        writeSheet writes data to the "processed" sheet
+        :param sheet:
+        :param editsheet:
+        :return:
+        """
+        row = 3
+        header = 'Required Annual Review Assessments- by %d / %d' % (month, year)
+        editsheet.write(0, 0, header, easyxf('font: height 400;'))
+        for c in range(0, sheet.ncols):
+            if c == 7:
+                editsheet.write(2, 0, sheet.cell_value(rowx=2, colx=c))
+            else:
+                editsheet.write(2, c+1, sheet.cell_value(rowx=2, colx=c))
+        for r in range(3, sheet.nrows):
+            if dateconvert(r, 3):
+                col = 0
+                for c in range(0, sheet.ncols):
+                    if (c == 4) and (sheet.cell_value(rowx=r, colx=c) != ''):
+                        if yearconvert(r, 4) and datecheck(dateconvert(r, 4)):
+                            editsheet.write(row, c+1, sheet.cell_value(rowx=r, colx=c),
+                                            easyxf('pattern: pattern solid_fill,fore_colour yellow;'))
+                            col += 1
+                        else:
+                            editsheet.write(row, c+1, sheet.cell_value(rowx=r, colx=c))
+                    elif c == 5:
+                        try:
+                            yearconvert(r, 5)
+                            dateconvert(r, 5)
+                        except ValueError:
+                            editsheet.write(row, c+1, "No Data Entered",
+                                            easyxf("pattern: pattern solid_fill, fore_colour yellow;"))
+                            col += 1
+                        else:
+                            if (yearconvert(r, 5) is False) and (dateconvert(r, 5)):
+                                editsheet.write(row, c+1, sheet.cell_value(rowx=r, colx=c),
+                                                easyxf('pattern: pattern solid_fill, fore_colour yellow;'))
+                                col += 1
+                            else:
+                                editsheet.write(row, c+1, sheet.cell_value(rowx=r, colx=c))
+                    elif c == 7:
+                        try:
+                            g = re.compile("([A-Z][A-Z])")
+                            p = re.search(g, sheet.cell_value(rowx=r, colx=c))
+                        except Exception:
+                            editsheet.write(row, 0, sheet.cell_value(rowx=r, colx=c))
+                        except AttributeError:
+                            editsheet.write(row, 0, sheet.cell_value(rowx=r, colx=c))
+                        else:
+                            g = re.compile("([A-Z][A-Z])")
+                            p = re.search(g, sheet.cell_value(rowx=r, colx=c))
+                            print(r)
+                            print(c)
+                            editsheet.write(row, 0, p.group())
+                    else:
+                        print("BONK!")
+                        editsheet.write(row, c+1, sheet.cell_value(rowx=r, colx=c))
+                        col += 1
+                row += 1
+            else:
+                print("OOOF!")
+
+    working_month()
+    working_year()
+    writesheet(sheet, editsheet)
+    wb.save(
+        asksaveasfilename(defaultextension='.xls', initialfile='Required Annual Reviews Assessments (Processed).xls')
+    )
+    print("Processing Complete")
